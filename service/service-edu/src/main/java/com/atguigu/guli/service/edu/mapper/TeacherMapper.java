abd216b6c329bace22ce27756ddc4e607df63c69
洛渚 <3330181534@qq.com>
2020-10-14 11:13:27 +0800

    guli基础架构配置+edu模块代码生成

---
+++ service/service-edu/src/main/java/com/atguigu/guli/service/edu/mapper/TeacherMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.guli.service.edu.mapper;
+
+import com.atguigu.guli.service.edu.entity.Teacher;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 讲师 Mapper 接口
+ * </p>
+ *
+ * @author abliger
+ * @since 2020-10-14
+ */
+public interface TeacherMapper extends BaseMapper<Teacher> {
+
+}
---
+++ service/service-edu/src/main/java/com/atguigu/guli/service/edu/service/TeacherService.java
@@ -0,0 +1,16 @@
+package com.atguigu.guli.service.edu.service;
+
+import com.atguigu.guli.service.edu.entity.Teacher;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 讲师 服务类
+ * </p>
+ *
+ * @author abliger
+ * @since 2020-10-14
+ */
+public interface TeacherService extends IService<Teacher> {
+
+}
